99e39b0251ab21c82ba0fd1b60bcc379234f1637
Antonio Gonzalez <antgonza@gmail.com>
2021-03-19 18:54:26 -0600

    add empty __init__.py

---
+++ qp_spades/__init__.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021, Qiita development team.
+#
+# Distributed under the terms of the BSD 3-clause License License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from qiita_client import QiitaPlugin, QiitaCommand
